a075c07516170e5769c7ad52ae906972b52941cd
Sebastien Perez <sebastien.perez@amadeus.com>
2015-10-20 15:34:02 +0200

    Fix downloadurl

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
   packages = ['SimpleSQLProxy'], # this must be the same as the name above
   install_requires=requirements,
   scripts=['bin/sqlproxyws'],
-  version = '0.3',
+  version = '0.4',
   description = 'A simple sqlproxy for SQL LITE databases based on flask',
   author = 'Sebastien Perez',
   author_email = 'sebastien.perezvasseur@gmail.com',
   url = 'https://github.com/rezpe/sqlproxy', # use the URL to the github repo
-  download_url = 'https://github.com/rezpe/sqlproxy/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/rezpe/sqlproxy/tarball/0.4', # I'll explain this in a second
   keywords = ['Web Service', 'SQL', 'sqlite'], # arbitrary keywords
   classifiers = [],
 )
